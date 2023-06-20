007786a9665d3ad6aa54b16ed970415efe9a7e65
bastosluis <lfb@alu.ufc.br>
2023-06-20 13:57:29 -0300

    Updated frontierAdvance.h

--- gato1/triangulation/frontierAdvance.h
+++ gato1/triangulation/frontierAdvance.h
@@ -6,6 +6,11 @@
 #include <utility>
 #include <vector>
 
+bool checkIntersection(const std::pair<int,int> edge, Vec2 vertex, std::vector<std::vector<int>> & adjacencyMatrix){
+    //checa se existe alguma interseção com alguma aresta antiga com as arestas geradas pela aresta atual e o vértice
+    //segmentIntersect do Vec2 checa pra gente se há interseção entre 2 arestas 
+}
+
 bool check(const std::pair<int,int> edge, std::vector<std::vector<int>> & adjacencyMatrix){
     adjacencyMatrix[edge.first][edge.second]++;
     adjacencyMatrix[edge.second][edge.first]++;
@@ -36,14 +41,21 @@ void frontierAdvance(std::vector<std::vector<int>> & adjacencyMatrix, std::vecto
     // vai esvaziando a fila, enquanto vai adicionando as novas arestas geradas 
     while (!queue.empty()){
         auto currentEdge = queue.front();
-        for (int i=0; i<adjacencyMatrix.size(); i++ ){
-            for (int j=0; j<adjacencyMatrix.size(); j++ ){
-                    //condição 1: o ponto está no lado esquerdo da aresta
-                    //condição 2: as novas arestas geradas não interceptam arestas existentes (circulo de delaunay)
-                    if (crossCompare(currentEdge.first, currentEdge.second,adjacencyMatrix[i][j]) && ){
-
-                    }
+        bool foundVertex = false;
+        int i = 0;
+        while(!foundVertex || i >= vertexList.size()){
+            //condição 1: o ponto está no lado esquerdo da aresta
+            //condição 2: as novas arestas geradas não interceptam arestas existentes (circulo de delaunay)
+            if (crossCompare(vertexList[currentEdge.first],vertexList[currentEdge.second], vertexList[i]) && !(checkIntersection(currentEdge, vertexList[i], adjacencyMatrix))){
+                check(currentEdge, adjacencyMatrix);
+                //check(std::pair<int,int> (currentEdge.first, i),adjacencyMatrix);
+                //check(std::pair<int,int> (i, currentEdge.second),adjacencyMatrix);
+                //queue.push() pra ambas as arestas
+                foundVertex = true;
             }
+            // invalida a aresta, pq existe interseção entre alguma coisa e ela
+            // invalidate(currentEdge, adjacencyMatrix);
+            i++;
         }
         
         if (isChecked(currentEdge, adjacencyMatrix)) {
